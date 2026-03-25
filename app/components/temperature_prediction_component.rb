a0769ecd83829f5c169161de95fcc6a766942ac6
Chris-cloud-ui <84877364+Chris-cloud-ui@users.noreply.github.com>
2026-03-25 16:23:36 +0000

    Update temperature_prediction_component.rb

--- app/components/temperature_prediction_component.rb
+++ app/components/temperature_prediction_component.rb
@@ -31,7 +31,7 @@ class TemperaturePredictionComponent < PredictionComponent
   end
 
   def level_label
-    if @value[:min] <= 12
+    if @value[:min] <= 2
       "Low temp"
     elsif @value[:max] >= 25
       "High temp"
