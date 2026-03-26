2a11d92fff1c4c669724f29e46be5d2e750a9b6d
Chris-cloud-ui <84877364+Chris-cloud-ui@users.noreply.github.com>
2026-03-26 09:35:48 +0000

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
+export RAILS_ENV=production
+yarn install --check-files || true
+
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
