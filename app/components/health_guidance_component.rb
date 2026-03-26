cc88ae8cf744c1477c1fe1d17f67fcf8eac97aa8
Chris-cloud-ui <84877364+Chris-cloud-ui@users.noreply.github.com>
2026-03-26 16:34:39 +0000

    Update health_guidance_component.rb

--- app/components/health_guidance_component.rb
+++ app/components/health_guidance_component.rb
@@ -2,15 +2,15 @@
 
 class HealthGuidanceComponent < ViewComponent::Base
   def initialize(type, level, classes: [])
-    @type = type&.to_sym
-    @level = level&.to_sym
+    @type = type
+    @level = level
     @classes = classes
-    @guidance_blocks = health_guidance[@level] || []
+    @guidance_blocks = health_guidance[level]
   end
 
   def health_guidance
     file = File.read("app/assets/guidance/health_guidance.json")
     data_hash = JSON.parse(file, symbolize_names: true)
-    data_hash[@type] || {}
+    data_hash[@type]
   end
 end
