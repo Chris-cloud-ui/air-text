5b8393b39ac53f9e2d517c876b116ba58d8b2dbd
Chris-cloud-ui <84877364+Chris-cloud-ui@users.noreply.github.com>
2026-03-27 13:27:15 +0000

    Update map_controller.js

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -198,7 +198,7 @@ export default class MapController extends Controller {
   addStreetMapLayer() {
     const streetMap = new MaptilerLayer({
       apiKey: this.settings.maptilerApiKey,
-      style: "13be04a7-d035-45cd-b5cf-6c0c50fdf6a8",
+      style: "019c0507-d879-7c46-8d06-83ae78c649ea",
       pane: "street-map",
     });
     this.map.addLayer(streetMap);
