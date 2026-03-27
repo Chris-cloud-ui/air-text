4764bcf65ade5a484405b1184b233cbe57cd729c
Chris-cloud-ui <84877364+Chris-cloud-ui@users.noreply.github.com>
2026-03-27 14:49:36 +0000

    Update map_controller.js

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -238,7 +238,7 @@ export default class MapController extends Controller {
       format: "image/png",
       opacity: 1,
       pane: "pollution",
-      styles: `daqi${pollutant}`,
+      styles: `daqi${pollcj}`,
     });
   }
 
