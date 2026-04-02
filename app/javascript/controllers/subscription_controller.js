302e501061fe2bc6264d72a94defb57eec6da3e6
Chris-cloud-ui <84877364+Chris-cloud-ui@users.noreply.github.com>
2026-04-02 11:51:59 +0100

    Update subscription_controller.js

--- app/javascript/controllers/subscription_controller.js
+++ app/javascript/controllers/subscription_controller.js
@@ -34,10 +34,10 @@ export default class SubscriptionController extends Controller {
   }
 
   resendVerification() {
-    const medium = this.resendVerificationButtonTarget.dataset.medium;
+    const mode = this.resendVerificationButtonTarget.dataset.medium;
     this.resendVerificationButtonTarget.textContent =
       "resend the code – sending...";
-    fetch(`/resend_verification_code?medium=${medium}`).then((response) => {
+    fetch(`/resend_verification_code?medium=${mode}`).then((response) => {
       if (response.ok) {
         this.resendVerificationButtonTarget.textContent =
           "resend the code – sent successfully";
