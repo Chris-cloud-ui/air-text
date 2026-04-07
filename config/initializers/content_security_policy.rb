35eda967488b232716d8bc74354408ab8be52940
Chris-cloud-ui <84877364+Chris-cloud-ui@users.noreply.github.com>
2026-04-07 11:12:01 +0100

    Update content_security_policy.rb

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -14,6 +14,8 @@ Rails.application.config.content_security_policy do |policy|
   policy.script_src :self, :https
   policy.worker_src :self, :blob
 
+  policy.style_src :self, :https
+
   # allow styling for the Turbo progress bar
   policy.style_src("'sha256-WAyOw4V+FqDc35lQPyRADLBWbuNK8ahvYEaQIYF1+Ps='", :self, :https)
 
@@ -22,13 +24,15 @@ Rails.application.config.content_security_policy do |policy|
 
   # Specify URI for violation reports
   # policy.report_uri "/csp-violation-report-endpoint"
+
+  
 end
 
 # If you are using UJS then enable automatic nonce generation
-# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
 
 # Set the nonce only to specific directives
-# Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
+Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
