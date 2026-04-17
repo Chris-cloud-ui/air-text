d2b8d46c702cf86b4a8cac49004a82f7487546cf
Chris-cloud-ui <84877364+Chris-cloud-ui@users.noreply.github.com>
2026-04-17 09:29:59 +0100

    Update subscription_form.rb

--- app/models/subscription_form.rb
+++ app/models/subscription_form.rb
@@ -191,7 +191,7 @@ class SubscriptionForm
           puts sms_number
           puts voice_number
           response = CercSubscriberApiClient.find_subscriber(
-            email: email
+            email: email,
             phonenumber: sms_number || voice_number
           )
           
